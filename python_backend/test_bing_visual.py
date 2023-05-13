bdc4ea995fb0a933897c0c5436c603bc4ef8aaea
Alexandru Arion <petre-alexandru.arion@continental-corporation.com>
2023-05-13 14:36:11 +0300

    dump json from bing image search

--- python_backend/test_bing_visual.py
+++ python_backend/test_bing_visual.py
@@ -4,7 +4,12 @@ BASE_URI = 'https://api.bing.microsoft.com/v7.0/images/visualsearch'
 SUBSCRIPTION_KEY = '438b8e05404840cd9726c5d9802f9f16'
 filePath = 'test_image.jpg'
 
+#formData = '{"knowledgeRequest":{"invokedSkills": ["SimilarImages"]}}'
+
+#file = {'knowledgeRequest': (None, formData)}
+
 file = {'image' : ('myfile', open(filePath, 'rb'))}
+
 
 HEADERS = {'Ocp-Apim-Subscription-Key': SUBSCRIPTION_KEY}
 
@@ -12,10 +17,18 @@ def print_json(obj):
     """Print the object as json"""
     print(json.dumps(obj, sort_keys=True, indent=2, separators=(',', ': ')))
 
+def save_json(obj, file_path):
+    with open(file_path, "w") as json_file:
+        json.dump(obj, json_file)
+
 try:
     response = requests.post(BASE_URI, headers=HEADERS, files=file)
     response.raise_for_status()
-    print_json(response.json())
+    #tags = response.json().tags
+    #print(len(tags))
+    #print_json(response.json())
+    #print(response.json()["tags"])
+    save_json(response.json(), "response.json")
     
 except Exception as ex:
     raise ex
